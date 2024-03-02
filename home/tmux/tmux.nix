ea87d38e55a9a551b8d85c8706da42025c5f46b6
Sébastien Hurtel <sebastienhurtel+github@gmail.com>
2024-03-02 15:41:08 +0100

    Use old tmux config file

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -1,59 +1,13 @@
 { ... }:
 
 {
+  home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
+  home.file.".config/tmux/tmux.local.conf".source = ./tmux.local.conf;
   programs.tmux = {
     enable = true;
     shell = "\${pkgs.zsh}/bin/zsh";
     terminal = "screen-256color";
     historyLimit = 10000;
     mouse = true;
-    extraConfig = ''
-      set -ga terminal-overrides ",xterm-256color:Tc"
-      set -s escape-time 10
-      set -sg repeat-time 600
-      set -s focus-events on
-      setw -q -g utf8 on
-      set -g base-index 1           # start windows numbering at 1
-      setw -g pane-base-index 1     # make pane numbering consistent with windows
-      setw -g automatic-rename on   # rename window to reflect current program
-      set-option -g automatic-rename-format '#{b:pane_current_path}'
-      set -g renumber-windows on    # renumber windows when a window is closed
-      set -g display-panes-time 800 # slightly longer pane indicators display time
-      set -g display-time 1000      # slightly longer status messages display time
-
-      # Custom bindings
-      bind C-c new-session
-      bind BTab switch-client -l
-      # split current window horizontally
-      bind - split-window -v
-      # split current window vertically
-      bind / split-window -h
-      bind p paste-buffer  # paste from the top paste buffer
-
-      # pane navigation
-      bind -r h select-pane -L  # move left
-      bind -r j select-pane -D  # move down
-      bind -r k select-pane -U  # move up
-      bind -r l select-pane -R  # move right
-      bind > swap-pane -D       # swap current pane with the next one
-      bind < swap-pane -U       # swap current pane with the previous one
-
-      # -- copy mode -----------------------------------------------------------------
-
-      bind Enter copy-mode # enter copy mode
-
-      run -b 'tmux bind -t vi-copy v begin-selection 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi v send -X begin-selection 2> /dev/null || true'
-      run -b 'tmux bind -t vi-copy C-v rectangle-toggle 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi C-v send -X rectangle-toggle 2> /dev/null || true'
-      run -b 'tmux bind -t vi-copy y copy-selection 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi y send -X copy-selection-and-cancel 2> /dev/null || true'
-      run -b 'tmux bind -t vi-copy Escape cancel 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi Escape send -X cancel 2> /dev/null || true'
-      run -b 'tmux bind -t vi-copy H start-of-line 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi H send -X start-of-line 2> /dev/null || true'
-      run -b 'tmux bind -t vi-copy L end-of-line 2> /dev/null || true'
-      run -b 'tmux bind -T copy-mode-vi L send -X end-of-line 2> /dev/null || true'
-    '';
   };
 }
