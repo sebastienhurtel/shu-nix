356e0f70a59c4b90663f1b6b8fda9d885843ea9f
shu <sebastienhurtel@gmail.com>
2024-03-07 23:30:52 +0100

    wip :)

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -1,13 +1,12 @@
 { ... }:
 
 {
-  home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
+  home.file.".config/tmux/shu-tmux.conf".source = ./shu-tmux.conf;
   home.file.".config/tmux/tmux.local.conf".source = ./tmux.local.conf;
   programs.tmux = {
     enable = true;
-    shell = "\${pkgs.zsh}/bin/zsh";
-    terminal = "screen-256color";
-    historyLimit = 10000;
-    mouse = true;
+    extraConfig = ''
+      source ~/.config/tmux/shu-tmux.conf
+    '';
   };
 }
