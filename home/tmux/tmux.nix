cda6145ae87b8fcd6f859c68cb986127447b483e
shu <sebastienhurtel@gmail.com>
2024-03-10 20:39:11 +0100

    [home] fix tmux.conf.local

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -2,12 +2,11 @@
 
 {
   home.file.".config/tmux/shu-tmux.conf".source = ./shu-tmux.conf;
-  home.file.".config/tmux/tmux.local.conf".source = ./tmux.local.conf;
+  home.file.".config/tmux/tmux.conf.local".source = ./tmux.conf.local;
   programs.tmux = {
     enable = true;
     extraConfig = ''
       source -q ~/.config/tmux/shu-tmux.conf
-      source -q ~/.config/tmux/tmux.conf.local
     '';
   };
 }
