1e14863a87bdc4653ca31c2f6b593e4c5bf5082f
shu <sebastienhurtel@gmail.com>
2024-03-12 16:42:42 +0100

    [home] dconf: update keybindings

--- home/dconf/dconf.nix
+++ home/dconf/dconf.nix
@@ -2,15 +2,25 @@
   dconf = {
     settings = {
       "org/gnome/desktop/wm/keybindings" = {
-        move-to-workspace-left=["<Shift><Super>k"];
-        move-to-workspace-right=["<Shift><Super>j"];
-        switch-to-workspace-1=["<Super>1"];
-        switch-to-workspace-2=["<Super>2"];
-        switch-to-workspace-3=["<Super>3"];
-        switch-to-workspace-4=["<Super>4"];
-        switch-to-workspace-left=["<Super>Page_Up"];
-        switch-to-workspace-right=["<Super>Page_Down"];
-        toggle-maximized=["<Super>Up"];
+        close = ["<Super>q"];
+        cycle-group = "disabled";
+        cycle-group-backward = "disabled";
+        maximize = "disabled";
+        minimize = "disabled";
+        move-to-workspace-1 = "disabled";
+        move-to-workspace-last = "disabled";
+        move-to-workspace-left = ["<Shift><Super>k"];
+        move-to-workspace-right = ["<Shift><Super>j"];
+        switch-group = "disabled";
+        switch-group-backward = "disabled";
+        switch-to-workspace-1 = ["<Super>1"];
+        switch-to-workspace-2 = ["<Super>2"];
+        switch-to-workspace-3 = ["<Super>3"];
+        switch-to-workspace-4 = ["<Super>4"];
+        switch-to-workspace-last = "disabled";
+        switch-to-workspace-left = ["<Super>Page_Up"];
+        switch-to-workspace-right = ["<Super>Page_Down"];
+        toggle-maximized = ["<Super>Up"];
       };
     };
   };
