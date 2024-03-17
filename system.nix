b587cffc3fde192d0d4b6d460fed56b6c05f0303
shu <sebastienhurtel@gmail.com>
2024-03-17 23:24:00 +0100

    [ssytem] ajust tlp baterie threshold

--- system.nix
+++ system.nix
@@ -92,8 +92,8 @@
        RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
 
        #Optional helps save long term battery health
-       START_CHARGE_THRESH_BAT0 = 55; # 40 and bellow it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 60; # 60 and above it stops charging
+       START_CHARGE_THRESH_BAT0 = 50; # 40 and bellow it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 65; # 60 and above it stops charging
 
       };
     };
