7f5d6ce6e27654a59d50d60e87690c0de3bcc59a
shu <sebastienhurtel@gmail.com>
2024-05-05 17:30:18 +0200

    [doom] bump in packages.el

--- home/emacs/doom.d/packages.el
+++ home/emacs/doom.d/packages.el
@@ -13,15 +13,14 @@
 (package! tree-edit
   :recipe (:host github :repo "ethan-leba/tree-edit"))
 
-(package! org-download :pin "19e166f0a8c539b4144cfbc614309d47a9b2a9b7")
-(package! org-modern :pin "537e6b75e38bc0eff083c390c257098c9fc9ab49")
-(package! svg-tag-mode :pin "0e0ea48799d8911ed6c1ef60565a20fd5cf3dae4")
-(package! smtpmail)
-
-(package! uuid)
-(package! git-auto-commit-mode)
 (package! bm)
+(package! git-auto-commit-mode)
 (package! logstash-conf :pin "ebc4731c45709ad1e0526f4f4164020ae83cbeff")
 (package! minimap)
-(package! protobuf-mode :pin "c862c1cab4d591963437774cc7d3f0a8e4bb1da1")
+(package! org-download :pin "19e166f0a8c539b4144cfbc614309d47a9b2a9b7")
+(package! org-modern :pin "33f694c57113418c7e739a824ff57dc9e7eb413c")
+(package! protobuf-mode :pin "2f6e7055952eeff2fef2f29edb092c97e7949793")
+(package! smtpmail)
+(package! svg-tag-mode :pin "a152bc90a7c9dc17112893a19ddf91078b909057")
+(package! uuid)
 (package! yang-mode :pin "4b4ab4d4a79d37d6c31c6ea7cccbc425e0b1eded")
