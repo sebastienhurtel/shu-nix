035284cc1549be189e6b3a5664c6755f95ba5801
= <=>
2024-06-02 12:37:27 +0200

    [age] rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  sebastien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJz4KjSHOW5LuUPn7kaOgvj/pZWg0EmPrqiS5N3Bx0X4";
+  sebastien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID29OciCljpOoDbhKiextIDShlq+hcd3MLvuiZm50tAh";
   squarepusher = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKTSzIZr/KS0Ry2Yt3ytRcvl3qUiF7VxIbeafmu7qfc";
   deftones = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyukD6dhooT96Vh70kNwMFgE+juZqtsfzCPAj0ol0r6";
   users = [ sebastien ];
