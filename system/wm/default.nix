c33580f2b0ca73fb4642cb6480dd497aa60171be
= <=>
2024-06-16 21:09:13 +0200

    [home] wm: remove useless function

--- system/wm/default.nix
+++ system/wm/default.nix
@@ -1,19 +1,18 @@
 { pkgs, config, lib, wm, ... }:
+with lib;
 let
   cfg = config.services.shu-wm;
   compositor = import ./wayland.nix { inherit config lib pkgs; };
   gnome = import ./gnome.nix { inherit pkgs; };
   hyprland = import ./hyprland.nix { inherit pkgs; };
 
-  mkWm = wm: {
-    gnome = lib.recursiveUpdate compositor gnome;
-    hyprland = lib.recursiveUpdate compositor hyprland;
+  shu-wm = {
+    gnome = recursiveUpdate compositor gnome;
+    hyprland = recursiveUpdate compositor hyprland;
     headless.services.xserver.enable = false;
   };
-
-  shu-wm = mkWm wm;
 in
 {
-  options.services.shu-wm.enable = lib.mkEnableOption "Enable window manager.";
-  config = lib.mkIf cfg.enable shu-wm.${wm};
+  options.services.shu-wm.enable = mkEnableOption "Enable window manager.";
+  config = mkIf cfg.enable shu-wm.${wm};
 }
