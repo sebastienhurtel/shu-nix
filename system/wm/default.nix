86c87158bd309b9aa301ab112b7ce4e7be5032c7
= <=>
2024-08-11 21:54:09 +0200

    [system/wm/default.nix] remove with lib

--- system/wm/default.nix
+++ system/wm/default.nix
@@ -5,7 +5,6 @@
   wm,
   ...
 }:
-with lib;
 let
   cfg = config.services.shuWm;
   compositor = import ./wayland.nix { inherit config lib pkgs; };
@@ -13,12 +12,12 @@ let
   hyprland = import ./hyprland.nix { inherit pkgs; };
 
   shuWm = {
-    gnome = recursiveUpdate compositor gnome;
-    hyprland = recursiveUpdate compositor hyprland;
+    gnome = lib.recursiveUpdate compositor gnome;
+    hyprland = lib.recursiveUpdate compositor hyprland;
     headless.services.xserver.enable = false;
   };
 in
 {
-  options.services.shuWm.enable = mkEnableOption "Enable window manager.";
-  config = mkIf cfg.enable shuWm.${wm};
+  options.services.shuWm.enable = lib.mkEnableOption "Enable window manager.";
+  config = lib.mkIf cfg.enable shuWm.${wm};
 }
