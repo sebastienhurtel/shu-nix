e7504c1a5e47dad12da6be9bfa1e5933ee8b9fee
= <=>
2024-08-17 17:51:17 +0200

    [system] syntax

--- system/squarepusher.nix
+++ system/squarepusher.nix
@@ -47,13 +47,14 @@
     printing.enable = true;
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       jack.enable = true;
       pulse.enable = true;
     };
   };
-
   hardware.pulseaudio.enable = false;
 
   virtualisation = {
