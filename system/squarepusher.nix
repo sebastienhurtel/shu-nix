be666f8052c3cb0fa9f0f0a7f00349208da54ace
= <=>
2024-09-07 22:28:01 +0200

    [system] libvirt: add ovmf

--- system/squarepusher.nix
+++ system/squarepusher.nix
@@ -93,8 +93,18 @@
     libvirtd = {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
         runAsRoot = true;
-        ovmf.enable = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
       };
     };
   };
