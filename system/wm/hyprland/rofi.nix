6e0ada59d75205a47616711c0e10627aa370763a
= <=>
2024-11-26 22:47:12 +0100

    [hyprland] add rofi

--- system/wm/hyprland/rofi.nix
+++ system/wm/hyprland/rofi.nix
@@ -6,13 +6,17 @@
 }:
 let
   cfg = config.shu.Rofi;
+  style = import ./style.nix { inherit config username lib; };
 in
 {
   options.shu.Rofi.enable = lib.mkEnableOption "Enable Shu Rofi";
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} = {
-      programs.rofi = {
-        enable = true;
+      programs = {
+        rofi = {
+          enable = true;
+          theme = style.rofi;
+        };
       };
     };
   };
