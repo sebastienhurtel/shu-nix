e63311229d96dc67864a60264c0a5ae3d67c8f2e
= <=>
2024-12-20 21:34:16 +0100

    [stylix] move module with others home modules

---
+++ modules/home/stylix/stylix.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.shu.stylix;
+in
+{
+  options.shu.stylix.enable = lib.mkEnableOption "Enable shuStylix";
+  config = lib.mkIf cfg.enable {
+    stylix = {
+      enable = true;
+      image = ../../../wallpaper.png;
+      polarity = "dark";
+      opacity.applications = 0.9;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/google-dark.yaml";
+      targets = {
+        alacritty.enable = false;
+        tmux.enable = false;
+        emacs.enable = false;
+        waybar.enable = false;
+        rofi.enable = false;
+      };
+
+      fonts = {
+        monospace = {
+          package = pkgs.meslo-lgs-nf;
+          name = "MesloLGS NF";
+        };
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "Cantarell";
+        };
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+
+        sizes = {
+          applications = 12;
+          terminal = 10;
+          desktop = 10;
+          popups = 10;
+        };
+      };
+
+      cursor = {
+        name = "Bibata-Modern-Ice";
+        package = pkgs.bibata-cursors;
+      };
+    };
+  };
+}
