8381e9b547f99e3eaccbd84c23feb98476cb7a0a
shu <sebastienhurtel+github@gmail.com>
2025-01-18 00:06:06 +0100

    [system:aphex] add var for sshkey

--- hosts/aphex/system.nix
+++ hosts/aphex/system.nix
@@ -1,5 +1,9 @@
-{ username, ... }:
-
+{ username, lib, ... }:
+let
+  sshKey = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXnexw7oCRiKGgbyyaoX5LnSJZeLl/P7/nI8i0DFIFd62xNx95KALDmXWNslVwJzBlnPwXqoNHol4hpJBwo15GVd1x+QOCTeo8P2mXlqBtpjee4JAu0ZRXopAWFtItX27PO7CiimMok85BtcExWHTntw5LUQtaK7bz/NB0B1RgolrxsHVmo7OXf9Y7FtXewkeZAtZzKIDZipTIeaw8Orpn14Su46fiabkni8g5pHEoskg7pRbyRbZ6QjUbdE8XCZOca0CfSOZoapoEtzeJSPomIPognHHpuGOuucGMai9iM2q15fLtTFoZH3JlQuaON8Rx0j5/2F59XzzeiBQI5PE/ Aphex"
+  ];
+in
 {
   networking = {
     wireless.enable = false;
@@ -17,14 +21,10 @@
   users.users = {
     root = {
       initialPassword = "ChangeMe!";
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXnexw7oCRiKGgbyyaoX5LnSJZeLl/P7/nI8i0DFIFd62xNx95KALDmXWNslVwJzBlnPwXqoNHol4hpJBwo15GVd1x+QOCTeo8P2mXlqBtpjee4JAu0ZRXopAWFtItX27PO7CiimMok85BtcExWHTntw5LUQtaK7bz/NB0B1RgolrxsHVmo7OXf9Y7FtXewkeZAtZzKIDZipTIeaw8Orpn14Su46fiabkni8g5pHEoskg7pRbyRbZ6QjUbdE8XCZOca0CfSOZoapoEtzeJSPomIPognHHpuGOuucGMai9iM2q15fLtTFoZH3JlQuaON8Rx0j5/2F59XzzeiBQI5PE/ Aphex"
-      ];
+      openssh.authorizedKeys.keys = sshKey;
     };
     "${username}" = {
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXnexw7oCRiKGgbyyaoX5LnSJZeLl/P7/nI8i0DFIFd62xNx95KALDmXWNslVwJzBlnPwXqoNHol4hpJBwo15GVd1x+QOCTeo8P2mXlqBtpjee4JAu0ZRXopAWFtItX27PO7CiimMok85BtcExWHTntw5LUQtaK7bz/NB0B1RgolrxsHVmo7OXf9Y7FtXewkeZAtZzKIDZipTIeaw8Orpn14Su46fiabkni8g5pHEoskg7pRbyRbZ6QjUbdE8XCZOca0CfSOZoapoEtzeJSPomIPognHHpuGOuucGMai9iM2q15fLtTFoZH3JlQuaON8Rx0j5/2F59XzzeiBQI5PE/ Aphex"
-      ];
+      openssh.authorizedKeys.keys = sshKey;
     };
   };
 }
