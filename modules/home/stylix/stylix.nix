cbdc14564d8064bca49aee669c57e353bd41fd8a
shu <sebastienhurtel+github@gmail.com>
2025-05-14 23:52:53 +0200

    [flake] fix stylix hm/nixos module logic

--- modules/home/stylix/stylix.nix
+++ modules/home/stylix/stylix.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  self,
   ...
 }:
 let
@@ -12,6 +13,39 @@ in
   options.shu.home.stylix.enable = lib.mkEnableOption "Enable shu home stylix";
   config = lib.mkIf cfg.enable {
     stylix = {
+      enable = true;
+      image = "${self}/wallpaper.png";
+      polarity = "dark";
+      opacity = {
+        applications = 0.9;
+        desktop = 0.5;
+      };
+      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/google-dark.yaml";
+      fonts = {
+        monospace = {
+          package = pkgs.meslo-lgs-nf;
+          name = "MesloLGS NF";
+        };
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "Cantarell";
+        };
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+        sizes = {
+          applications = 12;
+          terminal = 10;
+          desktop = 10;
+          popups = 10;
+        };
+      };
+      cursor = {
+        name = "Vanilla-DMZ-AA";
+        size = 24;
+        package = pkgs.vanilla-dmz;
+      };
       iconTheme = {
         enable = true;
         package = pkgs.papirus-icon-theme;
--- modules/wm/stylix.nix
+++ modules/wm/stylix.nix
@@ -2,8 +2,8 @@
   config,
   lib,
   pkgs,
-  self,
   stylix,
+  username,
   ...
 }:
 let
@@ -15,35 +15,10 @@ in
   config = lib.mkIf cfg.enable {
     stylix = {
       enable = true;
-      image = "${self}/wallpaper.png";
-      polarity = "dark";
-      opacity.applications = 0.9;
+      homeManagerIntegration.autoImport = true;
       base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/google-dark.yaml";
-      fonts = {
-        monospace = {
-          package = pkgs.meslo-lgs-nf;
-          name = "MesloLGS NF";
-        };
-        sansSerif = {
-          package = pkgs.dejavu_fonts;
-          name = "Cantarell";
-        };
-        serif = {
-          package = pkgs.dejavu_fonts;
-          name = "DejaVu Serif";
-        };
-        sizes = {
-          applications = 12;
-          terminal = 10;
-          desktop = 10;
-          popups = 10;
-        };
-      };
-      cursor = {
-        name = "Vanilla-DMZ-AA";
-        size = 24;
-        package = pkgs.vanilla-dmz;
-      };
+      image = config.home-manager.users.${username}.stylix.image;
+      polarity = "dark";
     };
   };
 }
