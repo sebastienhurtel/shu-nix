3dc8d5cf53cdc2873257b066e2a7589019fe83bb
shu <sebastienhurtel+github@gmail.com>
2025-08-22 20:42:58 +0200

    [home] emacs: fmt

--- modules/home/emacs/emacs.nix
+++ modules/home/emacs/emacs.nix
@@ -7,36 +7,35 @@
 let
   cfg = config.shu.home.emacs;
   emacs = pkgs.emacs-pgtk;
-  packages =
-    with pkgs.unstable;
-    [
-      black
-      dockerfile-language-server-nodejs
-      dockfmt
-      gomodifytags
-      gopls
-      gore
-      gotests
-      gotools
-      graphviz
-      isort
-      libxml2
-      nixd
-      nixfmt-rfc-style
-      pipenv
-      poetry
-      pyenv
-      pyright
-      rust-analyzer
-      shellcheck
-      shfmt
-      uv
-    ]
-    ++ [ emacs ];
+  packages = with pkgs.unstable; [
+    black
+    dockerfile-language-server-nodejs
+    dockfmt
+    gcc
+    gomodifytags
+    gopls
+    gore
+    gotests
+    gotools
+    graphviz
+    isort
+    libtool
+    libxml2
+    nixd
+    nixfmt-rfc-style
+    pipenv
+    poetry
+    pyenv
+    pyright
+    shellcheck
+    shfmt
+    uv
+  ];
   nodePackages = with pkgs.unstable.nodePackages_latest; [
     bash-language-server
   ];
-  pythonPackages = with pkgs.python313Packages; [
+  pythonPackages = with pkgs.python3Packages; [
+    nose2pytest
     pyflakes
     pytest
   ];
@@ -50,7 +49,7 @@ in
 {
   options.shu.home.emacs.enable = lib.mkEnableOption "Enable shu home emacs";
   config = lib.mkIf cfg.enable {
-    home.packages = packages ++ pythonPackages ++ nodePackages ++ emacsPackages;
+    home.packages = packages ++ pythonPackages ++ nodePackages ++ emacsPackages ++ [ emacs ];
     services.emacs = {
       enable = true;
       package = emacs;
