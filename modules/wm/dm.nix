ad556c08ca8f3bc178c2af33253f1a2b1b2fea05
shu <sebastienhurtel+github@gmail.com>
2026-02-27 22:35:58 +0100

    [sysmte] display manager don't use hyprland
    Too slow

--- modules/wm/dm.nix
+++ modules/wm/dm.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
@@ -18,28 +17,11 @@ let
     dbus.enable = true;
   };
 
-  hyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
-    execr-once = ${pkgs.zsh}/bin/zsh -c ${config.programs.regreet.package}/bin/regreet; ${config.programs.hyprland.package}/bin/hyprctl dispatch exit/hyprland
-    monitor=eDP-1, 1920x1200@60, 0x0, 1
-    monitor=, prefered, auto-left, 1
-    monitor=FALLBACK, 1920x1080@60, auto, 1
-    misc {
-        disable_hyprland_logo = true
-        disable_splash_rendering = true
-    }
-  '';
-
   greetd = {
     libinput.enable = true;
     greetd = {
       enable = true;
       restart = true;
-      settings = {
-        default_session = {
-          command = "${lib.getExe pkgs.hyprland} --config ${hyprlandConfig}";
-          user = "greeter";
-        };
-      };
     };
   };
 
