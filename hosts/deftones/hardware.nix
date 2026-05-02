2963ccccfe9d9521b8360cc20cf802257ed499ad
shu <sebastienhurtel+github@gmail.com>
2026-05-02 15:42:34 +0200

    [lint] remove unless comments

--- hosts/deftones/hardware.nix
+++ hosts/deftones/hardware.nix
@@ -122,12 +122,7 @@
 
   swapDevices = [ { device = "/dev/disk/by-uuid/dea4ac40-af5a-4b53-8a2f-6240608226eb"; } ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
