eca7e9b2dc221553309ebd09522ff77b711ea69d
shu <sebastienhurtel+github@gmail.com>
2026-05-02 15:23:23 +0200

    [module] bwrapped claude-code

--- modules/home/claude-code/claude-code.nix
+++ modules/home/claude-code/claude-code.nix
@@ -7,20 +7,37 @@
 let
   cfg = config.shu.home.claude-code;
   wrappedClaudeCode = pkgs.mkBwrapper {
+    imports = [
+      # Enables common desktop functionality (bind sockets for audio, display, dbus, mount
+      # directories for fonts, theming, etc.).
+      pkgs.bwrapperPresets.desktop
+    ];
     app = {
       package = pkgs.unstable.claude-code;
       runScript = "claude";
       env = {
         CLAUDE_CONFIG_DIR = "$HOME/.config/claude-code";
+        SHELL = "${pkgs.bash}/bin/bash";
       };
     };
     mounts = {
+      read = [
+        "/run/systemd/resolve/stub-resolv.conf"
+      ];
       readWrite = [
         "$PWD"
+        "$HOME/.config/claude-code"
       ];
     };
-    fhsenv.skipExtraInstallCmds = true;
-    sockets.x11 = false;
+    fhsenv.performDesktopPostInstall = false;
+    sockets = {
+      x11 = false;
+      pipewire = false;
+      pulseaudio = false;
+      wayland = false;
+    };
+    dbus.enable = false;
+    flatpak.enable = false;
   };
 in
 {
