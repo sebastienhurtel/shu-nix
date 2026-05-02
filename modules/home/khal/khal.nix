8bc162fc0897876fe9061c02393960e5f4b8b050
shu <sebastienhurtel+github@gmail.com>
2026-05-02 15:40:27 +0200

    [config] test khal

--- modules/home/khal/khal.nix
+++ modules/home/khal/khal.nix
@@ -9,8 +9,26 @@ in
 {
   options.shu.home.khal.enable = lib.mkEnableOption "Enable Shu Khal";
   config = lib.mkIf cfg.enable {
+    accounts.calendar = {
+      basePath = ".calendars";
+      accounts."shu" = {
+        name = "shu";
+        primary = true;
+        khal = {
+          enable = true;
+          addresses = [ "sebastienhurtel@gmail.com" ];
+        };
+      };
+    };
     programs.khal = {
       enable = true;
+      locale = {
+        timeformat = "%H:%M";
+        dateformat = "%Y-%m-%d";
+        longdateformat = "%Y-%m-%d";
+        datetimeformat = "%Y-%m-%d %H:%M";
+        longdatetimeformat = "%Y-%m-%d %H:%M";
+      };
     };
   };
 }
