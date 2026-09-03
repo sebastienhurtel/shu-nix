e889a1e46009952d0b591542ec027be1f3946da5
shu <sebastienhurtel+github@gmail.com>
2026-09-03 19:53:01 +0200

    [flake] Use noctalia cachix

--- flake.nix
+++ flake.nix
@@ -32,14 +32,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
+    # noctalia = {
+    #   url = "github:noctalia-dev/noctalia";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     nix-bwrapper.url = "github:Naxdy/nix-bwrapper";
   };
-
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
   outputs =
     inputs:
     with inputs;
